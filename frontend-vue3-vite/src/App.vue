4b69580fcb52a1c7f7408f26102cf9e0104c262a
Blake M <blake@marterella.com>
2023-07-19 01:31:42 -0400

    Add tailwindCSS

--- frontend-vue3-vite/src/App.vue
+++ frontend-vue3-vite/src/App.vue
@@ -4,19 +4,7 @@ import Pythagoras from './components/Pythagoras.vue';
 </script>
 
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> 
-    <router-link to="/about">About</router-link>
-  </nav>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Recursion Tech" />
   <Pythagoras></Pythagoras>
 </template>
 
