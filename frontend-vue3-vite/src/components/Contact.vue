cc0bbf74294f289bd009b4df8f02be0e0d87f7a0
Blake M <blake@marterella.com>
2023-08-23 14:37:49 -0400

    Fix the link to send an email to me on the website

--- frontend-vue3-vite/src/components/Contact.vue
+++ frontend-vue3-vite/src/components/Contact.vue
@@ -19,6 +19,11 @@ export default defineComponent({
       <h2 class="text-3xl lg:text-4xl text-neutral-white pb-5 px-4">
         Contact Us
       </h2>
+      <p class="px-8 md:w-3/4 lg:w-1/2 mx-auto text-neutral-grayish-blue text-sm lg:text-base leading-5 mb-4">
+        Connect with us for projects. Big or small. 
+        <br />
+        <a href="mailto:blake@recursion.tech">blake@recursion.tech</a>
+      </p>
     </div>
 
   </div>
